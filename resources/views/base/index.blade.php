140e4e263d646ee4c732694dee4cd31204be60ed
devs.n9 <devs.n9@gmail.com>
2016-05-18 20:46:46 +0300

    base controller and route

---
+++ resources/views/base/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layout')
+
+@section('content')
+<h2>Index Page</h2>
+@endsection
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Base Application</title>
+</head>
+<body>
+    <h1>Layout</h1>
+    <div class="container">
+        @yield('content')
+    </div>
+</body>
+</html>
