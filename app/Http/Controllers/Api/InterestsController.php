b26e722ad405e2f908cd4bf4013320f7cf180ec0
Kostya <Kostya>
2016-06-03 20:14:02 +0300

    some change

--- app/Http/Controllers/Api/InterestsController.php
+++ app/Http/Controllers/Api/InterestsController.php
@@ -7,7 +7,13 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
+//use models
+use App\Interests;
+
 class InterestsController extends Controller
 {
-    //
+    public funcion get()
+    {
+        
+    }
 }
