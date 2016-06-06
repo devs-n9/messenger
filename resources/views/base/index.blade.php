0b3c8345384218c838e2c73be93e92fb3efc39e0
devs.n9 <devs.n9@gmail.com>
2016-06-06 19:18:53 +0300

    auth login start page

--- resources/views/base/index.blade.php
+++ resources/views/base/index.blade.php
@@ -2,55 +2,45 @@
 
 @section('content')
 <div id="particles-js">
-        <div class="col-md-8"></div>
-        <div class="col-md-4 login-box">
-            <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
+        <div class="col-md-9"></div>
+        <div class="col-md-3 login-box">
+            <form role="form msg-form" method="POST" action="{{ url('/login') }}">
                 {{ csrf_field() }}
 
                 <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                    <label class="col-md-4 control-label">E-Mail Address</label>
-
-                    <div class="col-md-6">
-                        <input type="email" class="form-control" name="email" value="{{ old('email') }}"> 
+                    <label>E-Mail Address</label>
+                        <input type="email" class="form-control msg-input" name="email" value="{{ old('email') }}"> 
                         @if ($errors->has('email'))
                         <span class="help-block">
                             <strong>{{ $errors->first('email') }}</strong>
                         </span> 
                         @endif
-                    </div>
                 </div>
 
                 <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                    <label class="col-md-4 control-label">Password</label>
-
-                    <div class="col-md-6">
-                        <input type="password" class="form-control" name="password"> 
-                        @if ($errors->has('password'))
+                    <label>Password</label>
+                    <input type="password" class="form-control msg-input" name="password"> 
+                    @if ($errors->has('password'))
                         <span class="help-block">
                         <strong>{{ $errors->first('password') }}</strong>
                         </span> 
-                        @endif
+                    @endif
+                </div>
+
+                <div class="form-group">
+                    <div class="checkbox">
+                        <label>
+                            <input type="checkbox" name="remember"> Remember Me
+                        </label>
                     </div>
                 </div>
 
                 <div class="form-group">
-                    <div class="col-md-6 col-md-offset-4">
-                        <div class="checkbox">
-                            <label>
-                                <input type="checkbox" name="remember"> Remember Me
-                            </label>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="form-group">
-                    <div class="col-md-6 col-md-offset-4">
-                        <button type="submit" class="btn btn-primary">
+                        <button type="submit" class="btn btn-success msg-btn">
                             <i class="fa fa-btn fa-sign-in"></i>Login
                         </button>
 
                         <a class="btn btn-link" href="{{ url('/password/reset') }}">Forgot Your Password?</a>
-                    </div>
                 </div>
             </form>
         </div>
