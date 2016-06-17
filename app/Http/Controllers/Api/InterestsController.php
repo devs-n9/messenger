b34fc7bc7199104402219041eeafc7bf032611ff
Kostya <Kostya>
2016-06-17 21:02:46 +0300

    interests id, user id

--- app/Http/Controllers/Api/InterestsController.php
+++ app/Http/Controllers/Api/InterestsController.php
@@ -1,15 +1,12 @@
 <?php
 
 namespace App\Http\Controllers\Api;
-
-<<<<<<< HEAD
 use Illuminate\Http\Request;
 use App\Http\Requests;
-
 use App\Http\Controllers\Controller;
 
 //use models
-use App\Interests;
+use App\Model\Interests;
 
 class InterestsController extends Controller
 {
--- public/js/angular/controllers/interests.js
+++ public/js/angular/controllers/interests.js
@@ -1,6 +1,10 @@
 messenger.controller('InterestsController', function($scope, $http) {
     $http.get("/api/interests/get").success(function(data){
-        console.log(data);
+//        console.log(data);
         $scope.list = data;
     });
+    
+    $scope.showItem = function(i){
+        console.log (i);
+    }
 });
