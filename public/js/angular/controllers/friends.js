2fe594068c99a79e4357870186732b36b9e5d056
devs.n9 <devs.n9@gmail.com>
2016-07-13 19:53:53 +0300

    test invites

--- public/js/angular/controllers/friends.js
+++ public/js/angular/controllers/friends.js
@@ -15,7 +15,7 @@ messenger.controller('FriendsController', function($scope, $http, toaster) {
         console.log(invites);
         invites.forEach(function(i){
             console.log(i);
-            toaster.pop('mytype', i.name + " wants invite you.", 'Invite me pls', 60000, 'trustedHtml');
+            toaster.pop('mytype', i.name + " wants invite you.", '<img src="/img/upload/' + i.avatar + '" >', 60000, 'trustedHtml');
             $('#toast-container > .toast-info').css('background-image', 'url(/img/upload/' + i.avatar + ') !important');
         });
         
